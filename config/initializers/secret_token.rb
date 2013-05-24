14a2a0042734647a5be46a1f109b1558e7a007a6
amaharjan <amir.maharjan@gmail.com>
2013-05-24 10:07:57 +0545

    time tracker app created and comiited

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeTracker::Application.config.secret_token = 'b8efd27dc3d5c16f61c4543a61a90f4b30ecf368eb602dbd07916e3045e10b5b357d483213b8cf756d644a03c822119dbb537d2edced92606fa05b10d4cd86fd'
